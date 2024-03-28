42ff6b7d2de0f1fed2c00bdc8b55c9f08615b88e
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-03-28 14:58:58 -0500

    a

---
+++ snippets/csharp/Domain/Enums/CustomEnumerations.cs
@@ -0,0 +1,66 @@
+public abstract class Enumeration
+{
+    public static IEnumerable<TEnum> GetAll<TEnum>() where TEnum : Enumeration
+    {
+        var fields = typeof(TEnum).GetFields(BindingFlags.Static | BindingFlags.Public);
+
+        var result = fields.Select(f => f.GetValue(null)).Cast<TEnum>();
+
+        return result;
+    }
+}
+
+public abstract class Enumeration<T>(T value, [CallerMemberName] string name="") : Enumeration, IComparable
+{
+    public string Name { get; } = name;
+    public T Value { get; } = value;
+
+    public static Type ValueType => typeof(T);
+
+    public int CompareTo(object obj) => Name.CompareTo(((Enumeration<T>) obj).Name);
+
+    public override bool Equals(object obj)
+    {
+        if (obj is not Enumeration<T> other)
+        {
+            return false;
+        }
+
+        var typeMatches = GetType().Equals(obj.GetType());
+        var valueMatches = Name.Equals(other.Name);
+
+        return typeMatches && valueMatches;
+    }
+
+    public static bool operator ==(Enumeration<T> left, Enumeration<T> right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Enumeration<T> left, Enumeration<T> right)
+    {
+        return !left.Equals(right);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
+
+
+public class FriendEnum(uint value, [CallerMemberName] string name = "") : Enumeration<uint>(value, name)
+{
+    public static readonly FriendEnum Ryan = new(1);
+    public static readonly FriendEnum Tim = new(2);
+    public static readonly FriendEnum Dyl = new(3);
+    public static readonly FriendEnum Gura = new(4);
+}
+
+public class CarEnum(uint value, [CallerMemberName] string name = "") : Enumeration<uint>(value, name)
+{
+    public static readonly CarEnum Ford   = new(1);
+    public static readonly CarEnum Chevy  = new(2);
+    public static readonly CarEnum Toyota = new(3);
+    public static readonly CarEnum Honda  = new(4);
+}
